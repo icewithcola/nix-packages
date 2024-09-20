b42e9e1b6c592edd295556884d31348b1152d27d
icewithcola <me@kagurach.uk>
2024-09-20 09:33:46 +0800

    [+][fonts] HarmonyOS Sans

---
+++ pkgs/HarmonyOS-Sans-fonts/default.nix
@@ -0,0 +1,54 @@
+{ lib
+, stdenvNoCC
+, fetchurl
+, variants ? [ ]
+, suffix ? ""
+, unzip
+, ...
+} @ args:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "HarmonyOS-Sans-fonts${suffix}";
+  version = "20240619"; # Date published on huawei's site.
+
+  src = fetchurl {
+    url = "https://developer.huawei.com/images/download/next/HarmonyOS-Sans.zip";
+    hash = "sha256-yKyV83FWMfN4czbpaJVxwSroGKAFlxOnJjE2Bc4OuNM=";
+  };
+
+  _variants = map (variant: builtins.replaceStrings [ " " ] [ "" ] variant) variants;
+  
+  nativeBuildInputs = [ unzip ];
+  unpackPhase = ''
+  mkdir -p harmonyos-sans -x __MACOSX
+  unzip -d harmonyos-sans/ ${src}
+  mv ./harmonyos-sans/HarmonyOS\ Sans\  ./harmonyos-sans/hmsans
+  ''; # Source file has __MACOSX dir and space in the folder name
+
+  installPhase = ''
+    # There are only ttf fonts, and we move to a single directory
+    local out_font=$out/share/fonts/harmonyos-sans
+  '' + (if _variants == [ ] then ''
+    for folder in $(ls -d harmonyos-sans/hmsans/*/); do
+      install -m444 -Dt $out_font "$folder"/*.ttf
+    done
+  '' else ''
+    for variant in $_variants; do
+      install -m444 -Dt $out_font harmonyos-sans/hmsans/HarmonyOS_Sans_"$variant"/*.ttf
+    done
+  '');
+
+  meta = {
+    description = "Harmony OS Sans font";
+    homepage = "https://developer.huawei.com/consumer/cn/design/resource/";
+    longDescription = ''
+    Based on users' reading feedback on multiple terminals in different scenarios, 
+    a brand new default font — HarmonyOS Sans — has been designed for HarmonyOS with a variety of considerations, 
+    including the dimensions of different devices, usage scenarios, and different users' requirements 
+    for font size and weight due to differences in line-of-sight and angle-of-view.
+    '';
+    license = lib.licenses.unfree;
+    platforms = lib.platforms.all;
+    maintainers = with lib.maintainers; [ icewithcola ];
+  };
+}
